6923988e46d200acc2de82425b8a4583ae3e87e4
Cody Branson <codyboi7000@gmail.com>
2020-06-11 17:12:59 -0600

    new changes

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const recRouter = require("../recipes/recipe-router.js");
 const server = express();
 
 server.use(express.json());
-server.use("api/recipe", recRouter);
+server.use("/api/recipe", recRouter);
 
 server.use("/", (req, res) => {
   res.json("Your API is up and running!");
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json("This is from your rec router!");
+});
+
 module.exports = router;
